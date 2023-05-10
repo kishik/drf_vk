a5f153452a066c787e329bd896a54c234ddb92c3
kishik <kishik.maxim@gmail.com>
2023-05-10 07:38:00 +0300

    evening

--- drf_vk/urls.py
+++ drf_vk/urls.py
@@ -1,6 +1,8 @@
-from django.urls import include, path
+from django.template.defaulttags import url
+from django.urls import include, path, re_path
 from rest_framework import routers
 from quickstart import views
+from rest_framework.schemas import get_schema_view
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -12,5 +14,11 @@ router.register(r'friends', views.FriendsViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^openapi-schema', get_schema_view(
+        title="drf_vk",  # Title of your app
+        description="test api",  # Description of your app
+        version="1.0.0",
+        public=True,
+    ), name='openapi-schema'),
 ]
